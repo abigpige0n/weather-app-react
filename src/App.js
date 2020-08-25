dd97e03f8e7a4e96a0682558b942d98e63e0bec6
abigpige0n <66327250+abigpige0n@users.noreply.github.com>
2020-08-24 23:23:08 -0400

    Changed site text

--- src/App.js
+++ src/App.js
@@ -9,16 +9,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to my React test site!
         </h1>
         <h2><Weather /></h2>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/abigpige0n/weather-app-react"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check out the Github here!
         </a>
       </header>
     </div>
