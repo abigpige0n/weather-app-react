9e0f69b57b2e5e366f47c4d483974e2ce5dd9d92
abigpige0n <66327250+abigpige0n@users.noreply.github.com>
2020-08-24 23:17:52 -0400

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,11 @@
 import React from "react";
-import axios from "axios";
 import Loader from 'react-loader-spinner';
 
 export default function Weather () {
     return(
         <Loader
         type="Puff"
-        color="#red"
+        color="red"
         height={100}
         width={100}
      />
