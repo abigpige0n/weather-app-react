ccd4ce37fb5447cb1712fc8dd48b769ce40b19cf
abigpige0n <66327250+abigpige0n@users.noreply.github.com>
2020-09-07 15:04:54 -0400

    Created Dynamic Weather Icon

--- src/Weather.js
+++ src/Weather.js
@@ -15,7 +15,7 @@ export default function Weather(props) {
         wind: response.data.wind.speed,
         humidity: response.data.main.humidity,
         description: response.data.weather[0].description,
-        iconUrl: "http://openweathermap.org/img/wn/02n@2x.png",
+        iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
         date: new Date(response.data.dt * 1000),
     });
   }
