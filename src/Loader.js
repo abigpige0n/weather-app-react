886b59ece0c8ca1d2a910578b028c25680708c0f
abigpige0n <66327250+abigpige0n@users.noreply.github.com>
2020-09-06 22:19:13 -0400

    Re-name Loader.js

--- src/Loader.js
+++ src/Loader.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Loader from 'react-loader-spinner';
 
-export default function Weather () {
+export default function Loader () {
     return(
         <Loader
         type="Puff"
