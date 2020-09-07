cc7d17eb7288c15e7a1b62294df08bae0c62e8c5
abigpige0n <66327250+abigpige0n@users.noreply.github.com>
2020-09-07 14:43:01 -0400

    Create SearchResults component

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,10 +1,52 @@
 import React from "react";
 import "./styles.css";
+import FormattedDate from "./FormattedDate";
+import FormattedTime from "./FormattedTime";
 
 export default function SearchResults(props) {
   return (
     <div className="SearchResults">
-      
+      <div className="row" id="today-weather">
+          <div className="col-2" />
+          <div className="col-3">
+            <img
+              src={props.data.iconUrl}
+              id="icon"
+              alt={props.data.description}
+              width="150"
+            />
+          </div>
+          <div className="col-2">
+            <ul className="basic-stats">
+              <li>
+                <span className="temp" id="temp-now">
+                  {Math.round(props.data.temperature)}
+                </span>
+                <a href="" id="celsius-link" className="active">
+                  °C
+                </a>
+                |
+                <a href="" id="fahrenheit-link">
+                  °F
+                </a>
+              </li>
+              <li id="description">{props.data.description}</li>
+              <li id="date"><FormattedDate date={props.data.date} /></li>
+              <li id="time"><FormattedTime date={props.data.date} /></li>
+            </ul>
+          </div>
+          <div className="col-3">
+            <ul className="advanced-stats">
+              <li>
+                Humidity: <span id="humidity" />
+                {props.data.humidity} %
+              </li>
+              <li>
+                Wind:<span id="wind">{props.data.wind}</span> km/h
+              </li>
+            </ul>
+          </div>
+        </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -2,8 +2,7 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./styles.css";
 import Loading from "./Loading";
-import FormattedDate from "./FormattedDate";
-import FormattedTime from "./FormattedTime";
+import SearchResults from "./SearchResults";
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ready: false });
@@ -41,47 +40,7 @@ export default function Weather(props) {
             </div>
           </div>
         </form>
-        <div className="row" id="today-weather">
-          <div className="col-2" />
-          <div className="col-3">
-            <img
-              src={weatherData.iconUrl}
-              id="icon"
-              alt={weatherData.description}
-              width="150"
-            />
-          </div>
-          <div className="col-2">
-            <ul className="basic-stats">
-              <li>
-                <span className="temp" id="temp-now">
-                  {Math.round(weatherData.temperature)}
-                </span>
-                <a href="" id="celsius-link" className="active">
-                  °C
-                </a>
-                |
-                <a href="" id="fahrenheit-link">
-                  °F
-                </a>
-              </li>
-              <li id="description">{weatherData.description}</li>
-              <li id="date"><FormattedDate date={weatherData.date} /></li>
-              <li id="time"><FormattedTime date={weatherData.date} /></li>
-            </ul>
-          </div>
-          <div className="col-3">
-            <ul className="advanced-stats">
-              <li>
-                Humidity: <span id="humidity" />
-                {weatherData.humidity} %
-              </li>
-              <li>
-                Wind:<span id="wind">{weatherData.wind}</span> km/h
-              </li>
-            </ul>
-          </div>
-        </div>
+        <SearchResults data={weatherData} />
       </div>
     );
   } else {
