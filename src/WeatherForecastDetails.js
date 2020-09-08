a15ea7bea270a09923c30839872302e02d858d4a
abigpige0n <66327250+abigpige0n@users.noreply.github.com>
2020-09-07 21:53:57 -0400

    Style Weather Forecast

--- src/WeatherForecastDetails.js
+++ src/WeatherForecastDetails.js
@@ -3,15 +3,16 @@ import React from "react";
 export default function WeatherForecastDetails(props) {
     return(
         <div className="col">
+            <br />
             {new Date(props.data.dt * 1000).getHours()}:00
             <br />
             {props.data.weather[0].description}
             <br />
             {Math.round(props.data.main.temp)}°C
             <br />
-            {props.data.main.humidity}% Humidity
+            Humidity: {props.data.main.humidity}%
             <br />
-            {props.data.wind.speed} km/h
+            Wind: {props.data.wind.speed} km/h
         </div>
     );
 }
